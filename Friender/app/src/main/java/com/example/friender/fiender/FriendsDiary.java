b08e92a736d9cab1c677b68a1125c626da9cd11b
adamkocalek <adam.kocalek@gmail.com>
2017-11-21 15:14:04 +0100

    Parser + FriendsDiary Activity

---
+++ Friender/app/src/main/java/com/example/friender/fiender/FriendsDiary.java
@@ -0,0 +1,78 @@
+package com.example.friender.fiender;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.xml.sax.Parser;
+
+public class FriendsDiary extends AppCompatActivity {
+
+    ListView listView_new;
+    Parser parser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friendsDiary);
+        listView_new = (ListView) findViewById(R.id.listView_new);
+
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute("getFuels");
+
+        try {
+            Toast.makeText(getApplicationContext(), "Pobieram dane...", Toast.LENGTH_SHORT).show();
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+
+//        parser = new Parser(getApplicationContext(), backgroundWorker.tempJSON);
+//        parser.parseTanks();
+//
+//        ListViewAdapter lviewAdapter;
+//        lviewAdapter = new ListViewAdapter(this, parser.ids, parser.cars, parser.dates, parser.averages, parser.fuels, parser.kilometerss);
+//
+//
+//        listView_new.setAdapter(lviewAdapter);
+
+//        listView_new.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//
+//                Intent intent = new Intent(getContext(), ProjectActivity.class);
+//                TextView textView = (TextView) view.findViewById(R.id.textViewIds);
+//
+//                if (parser.ids.contains(textView.getText() + "")) {
+//                    position = parser.ids.indexOf(textView.getText() + "");
+//                } else {
+//                    position = -1;
+//                }
+//                if (position != -1) {
+//                    intent.putExtra("id", parser.ids.get(position));
+//                    intent.putExtra("car", parser.cars.get(position));
+//                    intent.putExtra("date", parser.dates.get(position));
+//                    intent.putExtra("average", parser.averages.get(position));
+//                    intent.putExtra("fuel", parser.fuels.get(position));
+//                    intent.putExtra("kilometers", parser.kilometerss.get(position));
+//                    getContext().startActivity(intent);
+//                }
+//            }
+//        });
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+//        parser.cars.clear();
+//        parser.dates.clear();
+//        parser.averages.clear();
+//        parser.fuels.clear();
+//        parser.kilometerss.clear();
+//        parser.ids.clear();
+        this.finish();
+    }
+}
---
+++ Friender/app/src/main/java/com/example/friender/fiender/Parser.java
@@ -0,0 +1,127 @@
+package com.example.friender.fiender;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class Parser extends AsyncTask<Void, Integer, Integer> {
+
+    Context context;
+    public static String data;
+    String taskName;
+    public static ArrayList<String> ids = new ArrayList<>(), names = new ArrayList<>(), ages = new ArrayList<>(), hobbies = new ArrayList<>(), sexs = new ArrayList<>(), pictures = new ArrayList<>(), logins = new ArrayList<>(), passwords = new ArrayList<>(), cities = new ArrayList<>(), reservations = new ArrayList<>(), id_users = new ArrayList<>(), ranks = new ArrayList<>();
+
+    ProgressDialog progressDialog;
+
+    public Parser() {
+
+    }
+
+    public Parser(Context context, String data) {
+        this.context = context;
+        this.data = data;
+    }
+
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setMessage("Przetwarzanie danych");
+        progressDialog.show();
+    }
+
+    @Override
+    protected Integer doInBackground(Void... params) {
+        return this.parseTanks();
+    }
+
+    @Override
+    protected void onPostExecute(Integer integer) {
+        super.onPostExecute(integer);
+        if (integer == 1) {
+            if (taskName != null) {
+                switch (taskName) {
+                    case "RootActivityIntent":
+                        Intent intent = new Intent(context, MainActivity.class);
+                        context.startActivity(intent);
+                        break;
+
+                    default:
+                }
+            }
+        } else {
+            Toast.makeText(context, "Nie można przetworzyć danych.", Toast.LENGTH_SHORT).show();
+        }
+        progressDialog.dismiss();
+    }
+
+    // Parsowanie pobranego JSON Array
+    int parseTanks() {
+        try {
+            // Dodanie danych do JSON Array
+            JSONArray ja = new JSONArray(data);
+
+            // Tworzenie pojedynczego JSON Object to przechowywania pojedynczego elementu
+            JSONObject jo = null;
+            names.clear();
+            ages.clear();
+            hobbies.clear();
+            sexs.clear();
+            pictures.clear();
+            logins.clear();
+            passwords.clear();
+            cities.clear();
+            reservations.clear();
+            id_users.clear();
+            ranks.clear();
+
+
+            for (int i = ja.length() - 1; i > -1; i--) {
+                jo = ja.getJSONObject(i);
+
+                // Odbieranie danych
+                String id = jo.getString("id");
+                String name = jo.getString("name");
+                String age = jo.getString("age");
+                String hobby = jo.getString("hobby");
+                String sex = jo.getString("sex");
+                String picture = jo.getString("pictures");
+                String login = jo.getString("login");
+                String password = jo.getString("password");
+                String city = jo.getString("city");
+                String reservation = jo.getString("reservation");
+                String id_user = jo.getString("id_user");
+                String rank = jo.getString("reank");
+
+                // Dodanie danych do normalnej listy
+                ids.add(id);
+                names.add(name);
+                ages.add(age);
+                hobbies.add(hobby);
+                sexs.add(sex);
+                pictures.add(picture);
+                logins.add(login);
+                passwords.add(password);
+                cities.add(city);
+                reservations.add(reservation);
+                id_users.add(id_user);
+                ranks.add(rank);
+            }
+            return 1;
+
+        } catch (JSONException e) {
+            Log.d("LOG Parser", e + "");
+        }
+        return 0;
+    }
+}
